b62b62198bd25ba6e382a112401784c1e1581650
ivall <ivallejo@elpuig.xeill.net>
2023-05-20 15:57:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/red_social/HomeFragment.java
@@ -0,0 +1,91 @@
+package com.example.red_social;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class HomeFragment extends Fragment {
+
+    NavController navController;
+    Button publishButton;
+    EditText postConentEditText;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        view.findViewById(R.id.gotoNewPostFragmentButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.newPostFragment);
+            }
+        });
+
+        publishButton = view.findViewById(R.id.publishButton);
+        postConentEditText = view.findViewById(R.id.postContentEditText);
+
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publicar();
+            }
+        });
+    }
+
+
+    private void publicar() {
+        String postContent = postConentEditText.getText().toString();
+
+        if(TextUtils.isEmpty(postContent)){
+            postConentEditText.setError("Required");
+            return;
+        }
+
+        publishButton.setEnabled(false);
+
+        guardarEnFirestore(postContent);
+    }
+
+    private void guardarEnFirestore(String postContent) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        Post post = new Post(user.getUid(), user.getDisplayName(), user.getPhotoUrl().toString(), postContent);
+
+        FirebaseFirestore.getInstance().collection("posts")
+                .add(post)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        navController.popBackStack();
+                    }
+                });
+    }
+}
